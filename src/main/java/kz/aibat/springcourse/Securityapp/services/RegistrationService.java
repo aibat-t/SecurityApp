261b573784edd184f1fdd636610112a659106052
Aibat Tozhybayev <tozhybayev.aibat@gmailcom>
2022-10-09 12:05:31 +0600

    Logout

--- src/main/java/kz/aibat/springcourse/Securityapp/services/RegistrationService.java
+++ src/main/java/kz/aibat/springcourse/Securityapp/services/RegistrationService.java
@@ -3,6 +3,7 @@ package kz.aibat.springcourse.Securityapp.services;
 import kz.aibat.springcourse.Securityapp.models.Person;
 import kz.aibat.springcourse.Securityapp.repositories.PeopleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +12,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistrationService {
 
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void register(Person person){
+        person.setPassword(passwordEncoder.encode( person.getPassword()));
         peopleRepository.save(person);
     }
 }
